1df5bcfdc84659052411273768057269171acd28
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-09-14 17:40:01 -0500

    nuevos cambios

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const {sequelize} = require('./src/db')
 
 
 app.listen(3001, () => {
-    sequelize.sync({force:true})
+    sequelize.sync({force:false})
     console.log('Server is running on port 3001...')
 })
