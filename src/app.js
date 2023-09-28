50242316dd2b0dd8d49bb892b049d2f712a4b38d
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-09-28 14:26:42 -0500

    cositas

--- src/app.js
+++ src/app.js
@@ -1,21 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const mainRouter = require('./routes')
-const pg = require('pg')
-require('dotenv').config()
-const {DATABASE_URL} = process.env
-
-const pool = new pg.Pool({
-    connectionString:DATABASE_URL,
-    ssl:true
-})
 
 const app = express()
 
-app.get('/ping', async(req, res) => {
-    const result = await pool.query('SELECT NOW()')
-    return res.json(result.rows[0])
-})
+
 
 app.use(express.json())
 app.use(morgan('dev'))
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,24 @@
 const { Router } = require('express')
 const useFeedback = require('./useFeedback')
 const usePost = require('./usePost')
+const pg = require('pg')
+require('dotenv').config()
+const {DATABASE_URL} = process.env
+
+const pool = new pg.Pool({
+    connectionString:DATABASE_URL,
+    ssl:true
+})
 
 const mainRouter = Router()
 
 mainRouter.use('/', (req, res) =>{
     res.status(201).send('Pagina funcionando')
+})
+
+mainRouter.get('/ping', async(req, res) => {
+    const result = await pool.query('SELECT NOW()')
+    return res.json(result.rows[0])
 })
 
 mainRouter.use('/post', usePost)
