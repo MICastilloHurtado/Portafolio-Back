1b7dabd28b160227cf24f2ef7012f5a9375c2e3c
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-10-01 15:42:14 -0500

    nuevo

--- src/db.js
+++ src/db.js
@@ -5,14 +5,10 @@ const feedBackModel = require('./models/feedBack.js')
 
 const {DB_USER, DB_PASSWORD, DB_HOST, DATABASE_URL } = process.env
 
-const local = true
-const sequelize = local === true
-? new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/portafolio`, {logging:false})
-: new Sequelize(`${DATABASE_URL}`,
-    { 
-      logging: false,
-       native: false,
-       })
+const sequelize = new Sequelize(DATABASE_URL || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/portafolio`, {
+  logging: false,
+  native: false,
+});
              
 
 postModel(sequelize)
