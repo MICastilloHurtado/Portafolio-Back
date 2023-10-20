e0364af193249f1eeca8e0b45e7c8472e0f3a116
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-10-20 18:07:36 -0500

    acceso

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ const mainRouter = require('./routes')
 
 const app = express()
 
-
-
 app.use(express.json())
 app.use(morgan('dev'))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
 app.use(mainRouter)
 
 module.exports = app;
